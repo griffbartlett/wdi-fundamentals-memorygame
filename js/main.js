6e70a43f63a018ba455f1e7827446a62d7485d1e
Griffin <griffinsbartlett@gmail.com>
2019-07-25 01:25:00 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,19 +1,33 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
-
-if (cardsInPlay.length === 2) {
+function checkForMatch() {
+	if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	}
 	if (cardsInPlay[0] !== cardsInPlay[1]) {
 		alert("Sorry, try again.");
-	}
+  }
 }
 
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+}
+function flipcard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
+
+flipcard(0);
+flipcard(2);
+
+};
+
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+}
+
+flipCard(0, 1);
+flipCard(2);
